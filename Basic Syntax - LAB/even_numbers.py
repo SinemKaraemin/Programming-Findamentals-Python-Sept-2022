8d3ba4372657585f3d73739f87e8684f340625ef
SinemKaraemin <sinemzkaraemin@gmail.com>
2022-09-22 14:15:25 +0300

    Add tasks
    Add conditions and solutions for problems in Basic Syntax - LAB - Softuni

---
+++ Basic Syntax - LAB/even_numbers.py
@@ -0,0 +1,10 @@
+n=int(input())
+
+for _ in range(1,n+1):
+    current_num=int(input())
+
+    if current_num%2!=0:
+        print(f'{current_num} is odd!')
+        break
+else:
+    print('All numbers are even.')
---
+++ Basic Syntax - LAB/largest_of_three_numbers.py
@@ -0,0 +1,2 @@
+num_1,num_2,num_3=int(input()),int(input()),int(input())
+print(max(num_1,num_2,num_3))
