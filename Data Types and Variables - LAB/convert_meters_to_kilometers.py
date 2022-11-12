d434723414a6860d9e2358d188a9b24d7e5e8021
SinemKaraemin <sinemzkaraemin@gmail.com>
2022-11-12 13:36:02 +0200

    Add tasks for lab
    Problem solutions for LAB - Data Types and Variables

---
+++ Data Types and Variables - LAB/convert_meters_to_kilometers.py
@@ -0,0 +1,4 @@
+distance_in_meters=int(input())
+
+kilometer=distance_in_meters/1000
+print(f'{kilometer:.2f}')
---
+++ Data Types and Variables - LAB/next_happy_year.py
@@ -0,0 +1,10 @@
+year = int(input())
+
+while True:
+    year += 1
+    current_year = str(year)
+    # current_year[0]!=current_year[1]!=current_year[2]!=current_year[3]:
+    pass
+    year=int(input())
+
+    print(current_year)
